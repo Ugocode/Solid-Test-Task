2ae349c06d0993c67ace0b7e8bc0fcf409230c52
ugocode <obiohaugo5@gmail.com>
2023-04-29 11:12:40 +0100

    added new constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,12 @@ const double textShimmer = 1000;
 
 ///constant for the emoji text
 const double emojiShimmerValue = 4500;
+
+///font size
+const double textSize = 35;
+
+///colorWidth
+const double colorWdith = 200;
+
+///Colorhight
+const double colorHeight = 100;
